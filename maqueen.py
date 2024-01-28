84b4f03d831e8f42f8620c9e039f4ca742bce885
Joshua Bas <joshua.n.bas@gmail.com>
2024-01-28 14:43:49 -0500

    added line sensor function; bit shifting not working as expected?

--- maqueen.py
+++ maqueen.py
@@ -18,9 +18,15 @@ RIGHT_LED:int               = const(1)
 LEFT_MOTOR:int              = const(0)
 RIGHT_MOTOR:int             = const(1)
 
-class Maqueen():
+LINE_SENSOR_L2:int          = const(0)
+LINE_SENSOR_L1:int          = const(1)
+LINE_SENSOR_M:int           = const(2)
+LINE_SENSOR_R1:int          = const(3)
+LINE_SENSOR_R2:int          = const(4)
 
-    I2C_ADDR                = const(0x10) # I just took this from the maqueen github repo
+class Maqueen():
+    # I just took this from the maqueen github repo
+    I2C_ADDR                = const(0x10)
     VERSION_CNT_REGISTER    = const(0x32)
     VERSION_DATA_REGISTER   = const(0x33)
     LEFT_LED_REGISTER       = const(0x0B)
@@ -33,6 +39,7 @@ class Maqueen():
     ADC2_REGISTER           = const(0x22)
     ADC3_REGISTER           = const(0x24)
     ADC4_REGISTER           = const(0x26)
+    LINE_STATE_REGISTER     = const(0x1D)
 
     is_initialized:bool = False
 
@@ -51,7 +58,7 @@ class Maqueen():
 
         Maqueen.is_initialized = True
 
-        microbit.i2c.write(Maqueen.I2C_ADDR, bytes([Maqueen.LEFT_LED_REGISTER, 0, 0]))
+        self.setLED(0)
         self.stopMotor()
         pass
 
@@ -75,6 +82,24 @@ class Maqueen():
     def stopMotor(self, motor:int|None=None) -> int:
         return self.setMotor(0, 0, motor)
 
+    def setLED(self, state:int, led:int|None=None) -> int:
+        if not Maqueen.is_initialized:
+            print("Not initialized!")
+            return -1
+
+        if led == LEFT_LED:
+            self.left_led_state = state
+            microbit.i2c.write(Maqueen.I2C_ADDR, bytes([Maqueen.LEFT_LED_REGISTER, self.left_led_state]))
+        elif led == RIGHT_LED:
+            self.right_led_state = state
+            microbit.i2c.write(Maqueen.I2C_ADDR, bytes([Maqueen.RIGHT_LED_REGISTER, self.right_led_state]))
+        else:
+            self.left_led_state = state
+            self.right_led_state = state
+            microbit.i2c.write(Maqueen.I2C_ADDR, bytes([Maqueen.LEFT_LED_REGISTER, self.left_led_state, self.right_led_state]))
+
+        return 0
+
     def toggleLED(self, led:int|None=None) -> int:
         if not Maqueen.is_initialized:
             print("Not initialized!")
@@ -95,8 +120,8 @@ class Maqueen():
 
     def setNeoPixel(self, rgb:tuple[int]|list[int], i:int|None=None) -> int:
         r, g, b = rgb
-        if not self.neopixel:
-            print("NeoPixels not initialized!")
+        if not Maqueen.is_initialized:
+            print("Not initialized!")
             return -1
         
         if r < 0 or r > 255 or g < 0 or g > 255 or b < 0 or b > 255:
@@ -117,8 +142,8 @@ class Maqueen():
         return 0
 
     def clearNeoPixel(self) -> int:
-        if not self.neopixel:
-            print("NeoPixels not initialized!")
+        if not Maqueen.is_initialized:
+            print("Not initialized!")
             return -1
 
         self.neopixel.clear()
@@ -128,6 +153,7 @@ class Maqueen():
         if not Maqueen.is_initialized:
             print("Not initialized!")
             return -1
+        
         microbit.pin13.write_digital(0)
         utime.sleep_us(5)
 
@@ -139,8 +165,73 @@ class Maqueen():
         d_cm = (0.034*t_us)/2 # sound in air: 340m/s
         return d_cm
 
-    def readLineSensor(self):
-        pass
+    def readLineSensorState(self, sensor:int|None=None) -> list[int]|int:
+        microbit.i2c.write(I2C_ADDR, bytes([LINE_STATE_REGISTER]))
+        raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+
+        # states_u = [
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x10),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x08),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x04),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x02),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x01)
+        # ]
+
+        # microbit.uart.write("Raw: " + str(states_u) + "   ")
+
+        states = [
+            int(int.from_bytes(raw_data_buffer, "little") & 0x10 == 0x10),
+            int(int.from_bytes(raw_data_buffer, "little") & 0x08 == 0x08),
+            int(int.from_bytes(raw_data_buffer, "little") & 0x04 == 0x04),
+            int(int.from_bytes(raw_data_buffer, "little") & 0x02 == 0x02),
+            int(int.from_bytes(raw_data_buffer, "little") & 0x01 == 0x01)
+        ]
+
+        # microbit.uart.write("Processed: " + str(states_c) + "   ")
+
+        # states = [
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x10 >> 4),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x08 >> 3),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x04 >> 2),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x02 >> 1),
+        #     bin(int.from_bytes(raw_data_buffer, "little") & 0x01 >> 0)
+        # ]
+
+        if sensor:
+            try:
+                return states[sensor]
+            except:
+                print("Not a valid sensor!")
+                return -1
+        else:
+            return states
+
+    def readLineSensorData(self, sensor:int) -> int:
+        if sensor == LINE_SENSOR_L2:
+            microbit.i2c.write(I2C_ADDR, bytes([ADC0_REGISTER]))
+            raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+            adc_data = raw_data_buffer[1] << 8 | raw_data_buffer[0]
+        elif sensor == LINE_SENSOR_L1:
+            microbit.i2c.write(I2C_ADDR, bytes([ADC1_REGISTER]))
+            raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+            adc_data = raw_data_buffer[1] << 8 | raw_data_buffer[0]
+        elif sensor == LINE_SENSOR_M:
+            microbit.i2c.write(I2C_ADDR, bytes([ADC2_REGISTER]))
+            raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+            adc_data = raw_data_buffer[1] << 8 | raw_data_buffer[0]
+        elif sensor == LINE_SENSOR_R1:
+            microbit.i2c.write(I2C_ADDR, bytes([ADC3_REGISTER]))
+            raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+            adc_data = raw_data_buffer[1] << 8 | raw_data_buffer[0]
+        elif sensor == LINE_SENSOR_R2:
+            microbit.i2c.write(I2C_ADDR, bytes([ADC4_REGISTER]))
+            raw_data_buffer = microbit.i2c.read(I2C_ADDR, 2)
+            adc_data = raw_data_buffer[1] << 8 | raw_data_buffer[0]
+        else:
+            print("Not a valid sensor!")
+            return -1
+        
+        return adc_data
 
 
     def _findI2CDevices(self):
@@ -157,3 +248,13 @@ class Maqueen():
             self.neopixel[i] = (0, 0, 0)
             self.neopixel.show()
         self.neopixel.clear()
+
+
+if __name__=="__main__":
+    robot = Maqueen()
+    microbit.uart.init(115200)
+
+    while True:
+        data = robot.readLineSensorState(LINE_SENSOR_M)
+        microbit.uart.write("Line sensor states: " + str(data) + "\r\n")
+        microbit.sleep(1000)
